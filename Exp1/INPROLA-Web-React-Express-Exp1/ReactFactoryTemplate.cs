7ae5d930bcc7c63f61824d189e035fe6016dc9fa
Jonathan Herrmann <jonathan.daniel.herrmann@gmail.com>
2019-12-01 20:48:02 +0100

    - Erste Schritte für React und Express Template gesetzt

---
+++ Exp1/INPROLA-Web-React-Express-Exp1/ReactFactoryTemplate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Inprola_EnvironmentFactoryTemplate;
+
+namespace INPROLA_Web_React_Express_Exp1
+{
+    public class ReactFactoryTemplate : EnvironmentFactoryTemplate
+    {
+        public void CreateEnvironment(string rootFolder)
+        {
+        }
+    }
+}
